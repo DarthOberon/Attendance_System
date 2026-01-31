50aace1ff60d235720a0ead95fa311b2a9011207
DarthOberon <10aryansharma@gmail.com>
2026-01-31 11:06:26 +0530

    Test 1 for recognizing face and marked attendance in csv

--- recognize_face.py
+++ recognize_face.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 import numpy as np
+import csv
+from datetime import datetime
 
 DATASET_DIR = "dataset"
 MODEL_PATH = "models"
@@ -40,6 +42,14 @@ cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
 print("Press Q or ESC to exit.")
 
+attendance_file = "attendance.csv"
+marked_present = set()
+
+if not os.path.exists(attendance_file):
+    with open(attendance_file, "w",newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Name","Date","Time","Status"])
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -48,20 +58,46 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     detected_faces = face_cascade.detectMultiScale(gray,1.3,5)
 
-    for (x,y,w,h) in detected_faces:
-        face = gray[y:y+h, x:x+w]
-        face = cv2.resize(frame, (200,200))
+    for (x, y, w, h) in detected_faces:
+        face_roi = frame[y:y+h, x:x+w]
 
-        label, confidence = recognizer.predict(face)
-        name = label_map.get(label,"Unknown")
+    # Safety check
+        if face_roi.size == 0:
+            continue
 
-        if confidence < 80:
+    # FORCE grayscale conversion HERE
+        face_gray = cv2.cvtColor(face_roi, cv2.COLOR_BGR2GRAY)
+        face_gray = cv2.resize(face_gray, (200, 200))
+
+    # EXTRA safety: ensure single channel
+        if len(face_gray.shape) != 2:
+            continue
+
+        label, confidence = recognizer.predict(face_gray)
+
+        name = label_map.get(label, "Unknown")
+
+        if confidence < 65:
             text = f"{name} ({confidence:.1f})"
+
+            if name not in marked_present:
+                now = datetime.now()
+                with open(attendance_file,"a",newline="") as f:
+                    writer = csv.writer(f)
+                    writer.writerow([
+                        name,
+                        now.strftime("%Y-%m-%d"),
+                        now.strftime("%H-%M-%S"),
+                        "Present"
+                    ])
+                marked_present.add(name)
+
         else:
             text = "Unknown"
 
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-        cv2.putText(frame,text,(x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0),2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
 
     cv2.imshow("Face Recognition - Press Q or ESC",frame)
 
